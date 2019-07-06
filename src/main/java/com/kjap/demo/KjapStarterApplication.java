d2e95ed95d2cda6dab3ed15c7181ef17f3c99b63
Ananth <Ananth@ASAA>
2019-07-06 09:40:14 -0400

    My greeting project

---
+++ src/main/java/com/kjap/demo/KjapStarterApplication.java
@@ -0,0 +1,13 @@
+package com.kjap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjapStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjapStarterApplication.class, args);
+	}
+
+}
